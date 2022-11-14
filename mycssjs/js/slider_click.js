260c4c82fd8ea8c793902da5be2ec0527ea5a93f
leo5678 <leoaaaleo@yahoo.com.tw>
2022-11-14 12:06:10 +0800

    fixbug, newpage

--- mycssjs/js/slider_click.js
+++ mycssjs/js/slider_click.js
@@ -4,163 +4,154 @@
 $(function () {
     var len = $("#slider div").length;
     var num = parseInt(len / 2);
-    var timelock = 1;
-    var imglock = 1;
-    var clicklock = 1;
+    var lockstate = 0;
     $("#slider div").css({ "left": `${(num - 1) * -520}px` })
     $("#slider div").eq(num).find("img").addClass("bright onlight").removeClass("dark");
-    
+
 
 
     // 點擊圖片
     $("#slider div img").click(function () {
-        if (timelock) {
-
-            timelock = 0;
+        if (lockstate == 0) {
             var index = $("#slider div img").index(this);
 
             // 點選發亮
             if (index == num) {
-                if (imglock) {
+                lockstate = 2;
+                $("#slider div").each(function () {
+                    $(this).addClass("slowmove")
+                    // 小於中間
+                    if ($("#slider div").index(this) < num) {
+                        $(this).css("left", "-=500")
+                    }
+                    // 大於於中間
+                    if ($("#slider div").index(this) > num) {
+                        $(this).css("left", "+=500")
+                    }
+                });
 
-                    $("#slider div").each(function () {
-                        $(this).addClass("slowmove")
-                        // 小於中間
-                        if ($("#slider div").index(this) < num) {
-                            $(this).css("left", "-=500")
-                        }
-                        // 大於於中間
-                        if ($("#slider div").index(this) > num) {
-                            $(this).css("left", "+=500")
-                        }
-                    });
+                // 中間圖放大
+                $("#slider div").eq(index).addClass("openimg")
 
-                    $("#slider div").eq(index).addClass("openimg")
+                $("#content").css("display" , "block")
 
-                    timelock = 1;
-                    imglock = 0;
-                }
+                setTimeout(function () {
+                    lockstate = 3;
+                }, 800)
             }
 
-            // 縮小圖片
-            $(".slider-outdiv").click(function () {
-                $(".slider-outdiv:not(.openimg)").click(function () {
-                    if (clicklock) {
-                        console.log("111")
-                        clicklock = 0
-                        setTimeout(function () {clicklock = 1;}, 800)
-                    }
-                })
-            })
+
 
 
             // 點選不發量
-            if (imglock) {
-                if (index != num) {
-                    // 本尊發光
-                    $("#slider div").eq(index).find("img").addClass("bright onlight");
-                    $("#slider div").eq(num).find("img").addClass("dark").removeClass("bright onlight");
+            if (index != num) {
+                lockstate = 1
+                // 本尊發光
+                $("#slider div").eq(index).find("img").addClass("bright onlight");
+                $("#slider div").eq(num).find("img").addClass("dark").removeClass("bright onlight");
 
-                    // 分身發光
+                // 分身發光
 
-                    $("#slider div").eq(num + len / 3).find("img").addClass("dark").removeClass("bright onlight");
-                    $("#slider div").eq(index + len / 3).find("img").addClass("bright onlight");
-                    $("#slider div").eq(num - len / 3).find("img").addClass("dark").removeClass("bright onlight");
-                    $("#slider div").eq(index - len / 3).find("img").addClass("bright onlight");
+                $("#slider div").eq(num + len / 3).find("img").addClass("dark").removeClass("bright onlight");
+                $("#slider div").eq(index + len / 3).find("img").addClass("bright onlight");
+                $("#slider div").eq(num - len / 3).find("img").addClass("dark").removeClass("bright onlight");
+                $("#slider div").eq(index - len / 3).find("img").addClass("bright onlight");
 
 
-                    // 畫面到另一邊
-                    if (index < len - len / 3) {
-                        index += len / 3
-                        num += len / 3
-                    }
-
-                    if (index >= len - len / 3) {
-                        index -= len / 3
-                        num -= len / 3
-                    }
-
-                    // 跳到分身的位置
-                    $("#slider div").css("left", `${(num - 1) * -520}px`)
+                // 畫面到另一邊
+                if (index < len - len / 3) {
+                    index += len / 3
+                    num += len / 3
                 }
 
-                // 左移
-                if (num > index) {
-                    if (num == index + 1) {
-                        $("#slider div").each(function () {
-                            $(this).addClass("moveleft")
-                        });
-
-                        setTimeout(function () {
-                            $("#slider div").each(function () {
-                                $(this).removeClass("moveleft")
-                            });
-
-                            $("#slider div").css("left", `${(index - 1) * -520}px`)
-
-                            timelock = 1;
-                        }, 800)
-                    }
-
-                    if (num == index + 2) {
-                        $("#slider div").each(function () {
-                            $(this).addClass("moveleft2")
-                        });
-
-                        setTimeout(function () {
-                            $("#slider div").each(function () {
-                                $(this).removeClass("moveleft2")
-                            });
-
-                            $("#slider div").css("left", `${(index - 1) * -520}px`)
-
-                            timelock = 1;
-                        }, 800)
-                    }
-
+                if (index >= len - len / 3) {
+                    index -= len / 3
+                    num -= len / 3
                 }
 
-                // 右移
-                if (num < index) {
-                    if (num == index - 1) {
-                        // console.log("start");
-                        $("#slider div").each(function () {
-                            $(this).addClass("moveright")
-                        });
-
-                        // console.log("removed")
-                        setTimeout(function () {
-                            $("#slider div").each(function () {
-                                $(this).removeClass("moveright")
-                            });
-                            $("#slider div").css("left", `${(index - 1) * -520}px`)
-
-                            timelock = 1;
-                        }, 800)
-                    }
-
-                    if (num == index - 2) {
-                        // console.log("start");
-                        $("#slider div").each(function () {
-                            $(this).addClass("moveright2")
-                        });
-
-                        // console.log("removed")
-                        setTimeout(function () {
-                            $("#slider div").each(function () {
-                                $(this).removeClass("moveright2")
-                            });
-
-                            $("#slider div").css("left", `${(index - 1) * -520}px`)
-
-                            timelock = 1;
-                        }, 800)
-                    }
-
-                }
-
-                num = index;
+                // 跳到分身的位置
+                $("#slider div").css("left", `${(num - 1) * -520}px`)
             }
+
+            // 左移
+            if (num > index) {
+                lockstate = 1
+                if (num == index + 1) {
+                    $("#slider div").each(function () {
+                        $(this).addClass("moveleft")
+                    });
+
+                    setTimeout(function () {
+                        $("#slider div").each(function () {
+                            $(this).removeClass("moveleft")
+                        });
+
+                        $("#slider div").css("left", `${(index - 1) * -520}px`)
+
+                        lockstate = 0;
+                    }, 800)
+                }
+
+                if (num == index + 2) {
+                    $("#slider div").each(function () {
+                        $(this).addClass("moveleft2")
+                    });
+
+                    setTimeout(function () {
+                        $("#slider div").each(function () {
+                            $(this).removeClass("moveleft2")
+                        });
+
+                        $("#slider div").css("left", `${(index - 1) * -520}px`)
+
+                        lockstate = 0;
+                    }, 800)
+                }
+
+            }
+
+            // 右移
+            if (num < index) {
+                lockstate = 1
+                if (num == index - 1) {
+                    // console.log("start");
+                    $("#slider div").each(function () {
+                        $(this).addClass("moveright")
+                    });
+
+                    // console.log("removed")
+                    setTimeout(function () {
+                        $("#slider div").each(function () {
+                            $(this).removeClass("moveright")
+                        });
+                        $("#slider div").css("left", `${(index - 1) * -520}px`)
+
+                        lockstate = 0;
+                    }, 800)
+                }
+
+                if (num == index - 2) {
+                    // console.log("start");
+                    $("#slider div").each(function () {
+                        $(this).addClass("moveright2")
+                    });
+
+                    // console.log("removed")
+                    setTimeout(function () {
+                        $("#slider div").each(function () {
+                            $(this).removeClass("moveright2")
+                        });
+
+                        $("#slider div").css("left", `${(index - 1) * -520}px`)
+
+                        lockstate = 0;
+                    }, 800)
+                }
+
+            }
+
+            num = index;
+
         }
     })
 })
