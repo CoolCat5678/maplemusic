a684db1a2d45d46e310df0891c53cd4526e883d0
leo5678 <leoaaaleo@yahoo.com.tw>
2022-11-16 10:24:04 +0800

    loading animation

--- mycssjs/js/homelogin.js
+++ mycssjs/js/homelogin.js
@@ -1,24 +1,27 @@
 $(function () {
     setTimeout(function () {
-        $("#homelogin h1").css("color", "rgba(255,255,255,0)")
+        $("#homelogin h1").css("color", "#E03C8A")
         setTimeout(function () {
-            $("#homelogin h1").text("Click")
-            $("#homelogin h1").css("color", "#E03C8A")
+            $("#homelogin h1").css("color", "rgba(255,255,255,0)")
             setTimeout(function () {
-                $("#homelogin").click(function () {
-                    setTimeout(function () {
-                        $("#homelogin h1").css("color", "rgba(255,255,255,0)")
-                    }, 100)
+                $("#homelogin h1").text("Click")
+                $("#homelogin h1").css("color", "#E03C8A")
+                setTimeout(function () {
+                    $("#homelogin").click(function () {
+                        setTimeout(function () {
+                            $("#homelogin h1").css("color", "rgba(255,255,255,0)")
+                        }, 100)
 
-                    setTimeout(function () {
-                        $("#homelogin").css("background-color", "rgba(0, 0, 0, 0)")
-                    }, 1100)
+                        setTimeout(function () {
+                            $("#homelogin").css("background-color", "rgba(0, 0, 0, 0)")
+                        }, 1100)
 
-                    setTimeout(function () {
-                        $("#homelogin").css("display", "none")
-                    }, 2100)
+                        setTimeout(function () {
+                            $("#homelogin").css("display", "none")
+                        }, 2100)
+                    })
                 })
-            })
-        }, 1000)
+            }, 1000)
+        }, 1300)
     }, 500)
 })
